7dcd60bc4932d21f6034c26d7768e120dd1c7ece
Giancarlo Ma <giancarlo961204@gmail.com>
2020-05-16 19:01:20 +0800

    car fueling problem

--- part1/third_week/car_fueling.cpp
+++ part1/third_week/car_fueling.cpp
@@ -8,7 +8,16 @@ using std::max;
 
 int compute_min_refills(int dist, int tank, vector<int> & stops) {
     // write your code here
-    return -1;
+    int curPos = 0;
+    int numRefills = 0;
+    int n = stops.size();
+    while(curPos < n - 1) {
+        int lastPos = curPos;
+        while(curPos < n - 1 && stops[curPos+1] -stops[lastPos] <= tank) curPos++;
+        if(curPos == lastPos) return -1;
+        if(curPos < n - 1) numRefills++;
+    }
+    return numRefills;
 }
 
 
@@ -20,10 +29,11 @@ int main() {
     int n = 0;
     cin >> n;
 
-    vector<int> stops(n);
-    for (size_t i = 0; i < n; ++i)
+    vector<int> stops(n+2);
+    for (size_t i = 1; i < n+1; ++i)
         cin >> stops.at(i);
-
+    stops[0] = 0;
+    stops[n+1] = d;
     cout << compute_min_refills(d, m, stops) << "\n";
 
     return 0;
