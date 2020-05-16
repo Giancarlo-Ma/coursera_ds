670e37f7c117497c9c706bc0b6c944ca22c60bcf
Giancarlo Ma <giancarlo961204@gmail.com>
2020-05-16 16:20:39 +0800

    change && knapsack problem

--- part1/third_week/change.cpp
+++ part1/third_week/change.cpp
@@ -2,6 +2,15 @@
 
 int get_change(int m) {
   //write your code here
+  int n = 0;
+  int denominations[] = {10, 5, 1};
+  for(int i = 0; i < 3; ++i) {
+    while(m >= denominations[i]) {
+      m -= denominations[i];
+      n++;
+    }
+    if(m == 0) break;
+  }
   return n;
 }
 
--- part1/third_week/fractional_knapsack.cpp
+++ part1/third_week/fractional_knapsack.cpp
@@ -1,29 +1,72 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-using std::vector;
+using namespace std;
 
-double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
-  double value = 0.0;
+struct Item
+{
+	int value;
+	int weight;
+};
 
-  // write your code here
-
-  return value;
+bool cmp(struct Item a, struct Item b)
+{
+	double r1 = (double)a.value  / a.weight;
+	double r2 = (double)b.value  / b.weight;
+	return r1 > r2;
 }
 
-int main() {
-  int n;
-  int capacity;
-  std::cin >> n >> capacity;
-  vector<int> values(n);
-  vector<int> weights(n);
-  for (int i = 0; i < n; i++) {
-    std::cin >> values[i] >> weights[i];
-  }
+double get_optimal_value(int capacity, vector<int> weights, vector<int> values)
+{
+	double value = 0.0;
 
-  double optimal_value = get_optimal_value(capacity, weights, values);
+	// write your code here
+	// construct [[value,weight]]
+	int n = weights.size();
+	Item items[n];
+	for(int i = 0; i != n; i++) {
+		items[i].value = values[i];
+		items[i].weight = weights[i];
+	}
 
-  std::cout.precision(10);
-  std::cout << optimal_value << std::endl;
-  return 0;
+	sort(items, items+n,cmp);
+	// for (int i = 0; i < n; i++) 
+    // { 
+    //     cout << items[i].value << "  " << items[i].weight << " : " 
+    //          << ((double)items[i].value / items[i].weight) << endl; 
+    // }
+
+	int curWeight = 0;
+	for(int i = 0; i < n; i++) {
+		if(curWeight + items[i].weight <= capacity) {
+			curWeight += items[i].weight;
+			value+=items[i].value;
+		}
+		else {
+			int remain = capacity - curWeight;
+			value+=items[i].value * ((double)remain / items[i].weight);
+			break;
+		}
+	}
+	return value;
+}
+
+int main()
+{
+	int n;
+	int capacity;
+	std::cin >> n >> capacity;
+	vector<int> values(n);
+	vector<int> weights(n);
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> values[i] >> weights[i];
+	}
+
+	double optimal_value = get_optimal_value(capacity, weights, values);
+
+	std::cout.precision(10);
+	std::cout << optimal_value << std::endl;
+	return 0;
 }
